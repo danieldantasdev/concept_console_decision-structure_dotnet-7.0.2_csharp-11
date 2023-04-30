115beee3ae21cfd53b772bee6ee23c556c8f6869
lobinhodev <contatolobinhodev@gmail.com>
2023-04-30 16:51:28 -0300

    init: commit initial

---
+++ Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter a string of numbers separated by spaces: ");
+// 0 1 2 3 4 5 6 7 8 9 10
+var numbers = Console.ReadLine().Split(" ");
+
+//for
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+//foreach
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+//while
+var cont = 0;
+
+while (cont < numbers.Length)
+{
+    Console.WriteLine(numbers[cont]);
+    cont++;
+}
+
+Console.ReadKey();
